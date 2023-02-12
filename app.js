16916b67ee703ac9f67653727d3f0f287c267db4
Kierro DeCampanille <pentawick@gmail.com>
2023-02-13 00:23:34 +0800

    still working on the collapse

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 
 const handleCollapseGroup=(e)=>{
-    console.log("handling");
-    console.log(e);
+    if(e.style.height != "auto") 
+    {
+        console.log(e.id+"-icon");
+        document.getElementById(e.id+"-icon").style.rotate= "180deg";
+        e.style.height="auto";
+    }
+    else {
+        
+        e.style.height = "1.2em";
+        document.getElementById(e.id+"-icon").style.rotate= "0deg";
+    }
 }
