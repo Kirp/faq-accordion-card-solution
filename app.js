c1e7877cfddc435d846b3bc765c048576193832d
Kierro DeCampanille <pentawick@gmail.com>
2023-02-13 23:52:58 +0800

    mobile design done

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const handleCollapseGroup=(e)=>{
     if(e.style.height != "auto") 
     {
-        console.log(e.id+"-icon");
+        document.getElementById(e.id+"-header").style.fontWeight = "700";
         document.getElementById(e.id+"-icon").style.rotate= "180deg";
         e.style.height="auto";
     }
     else {
-        
+        document.getElementById(e.id+"-header").style.fontWeight = "400";
         e.style.height = "1.2em";
         document.getElementById(e.id+"-icon").style.rotate= "0deg";
     }
